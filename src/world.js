7388cc1aab6643204b20b83a7047b803ea997547
lagos.d@gmail.com <lagos.d@gmail.com>
2018-12-21 15:04:35 -0800

    added lightbuld app

---
+++ src/world.js
@@ -0,0 +1,13 @@
+import React, { Component} from 'react'
+
+class World extends Component {
+  render(){
+    return(
+      <div>
+        <p> this is my world, toma carajo!!!</p>
+      </div>
+    )
+  }
+}
+
+export default World
